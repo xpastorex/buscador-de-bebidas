869f48523be5c5c53b4dfdc01940d12e2d5a8c6a
MatiasCamposB <pastorexcampos@gmail.com>
2025-01-03 15:59:59 -0300

    Navbar Terminada

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import IndexPage from "./views/IndexPage";
+import FavoritesPage from "./views/FavoritesPage";
+import Layout from "./layouts/Layout";
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexPage />} index/>
+          <Route path="/favoritos" element={<FavoritesPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/views/FavoritesPage.tsx
@@ -0,0 +1,8 @@
+
+export default function FavoritesPage() {
+  return (
+    <>
+    <h1>Favoritos</h1>
+    </>
+  )
+}
